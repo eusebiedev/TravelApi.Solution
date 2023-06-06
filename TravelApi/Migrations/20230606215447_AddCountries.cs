1c0a760d65994a4b6cc70433fcad4008bfa54f15
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-06 15:09:24 -0700

    Add new countries to database

---
+++ TravelApi/Migrations/20230606215447_AddCountries.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TravelApi.Migrations
+{
+    public partial class AddCountries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "CountryId", "Climate", "Language", "Name", "Population" },
+                values: new object[] { 8, "Desert Island Vibe", "Nut", "Coconut Land", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Countries",
+                columns: new[] { "CountryId", "Climate", "Language", "Name", "Population" },
+                values: new object[] { 9, "Glossy", "Sharpie", "Magnet Calendar", 7600 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Countries",
+                keyColumn: "CountryId",
+                keyValue: 9);
+        }
+    }
+}
